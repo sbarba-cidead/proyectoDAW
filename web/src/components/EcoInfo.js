16dda799d4984693f591521041fb1c7d726e1967
sbarba-cidead <sbarba-cidead@gmail.com>
2025-05-12 00:26:38 +0200

    Página EcoInfo

---
+++ web/src/components/EcoInfo.js
@@ -0,0 +1,11 @@
+import '../styles/EcoInfo.css';
+
+function EcoInfo() {
+    return (
+        <div className="main-container">
+            Esta página está por hacer. Se hará lunes 12/05.
+        </div>
+    );
+}
+
+export default EcoInfo;
